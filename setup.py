40cc3e059af2de937d165eb5cb252f85c54c46b8
Dana Powers <dana.powers@rd.io>
2015-03-16 16:56:49 -0700

    Patch release v0.0.1.1 -- for 0.90 stats

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='vor',
-      version='0.0.1',
+      version='0.0.1.1',
       description='Services for gatheric metrics to send to Graphite',
       maintainer='Ralph Meijer',
       maintainer_email='ralphm@ik.nu',
